9d891e754118b85eaebbf0ff124eb2772ac076d3
jiaoguotao <1234qwer!>
2024-10-21 19:08:50 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,82 @@
+# 常用工具函数
+
+## 安装
+
+`npm install utils-vue`
+
+## 目录
+
+| 方法名                     | 说明                   | 参数                      | 版本   |
+|-------------------------|----------------------|-------------------------|------|
+| isEmpty                 | 检查字符串是否为空            | (string):boolean        | 1.0  |
+| hexToRgba               | 颜色格式转换：hex格式转为rgba格式 | (string, number):string | 1.0  |
+| randomNum               | 生成指定范围随机数            | (number, number):number | 1.0  |
+| setPercentileSeparation | 数字转为千分位分隔            | (number):string         | 1.0  |
+| fistLetterUpper         | 字符串首字母大写             | (string):string         | 1.0  |
+| telFormat               | 手机号中间四位变成*           | (number或string):string  | 1.0  |
+
+## 详细
+
+### isEmpty(string)
+检查字符串是否为
+- 示例
+```javascript
+isEmpty('')
+// true
+
+isEmpty('0')
+// false
+
+isEmpty('1')
+// false
+
+isEmpty(null)
+// true
+
+isEmpty(undefined)
+// true
+```
+
+### hexToRgba(string, number)
+颜色格式转换：hex格式转为rgba格式
+- 示例
+```javascript
+hexToRgba('#00000', 0.8)
+// 'rgba(0, 0, 0, 0.8)'
+```
+
+### randomNum(number, number)
+生成指定范围随机数
+- 示例
+```javascript
+randomNum(0, 10000)
+// 5560 8264 4560
+```
+
+### setPercentileSeparation(number)
+数字转为千分位分隔,不支持支持小数
+- 示例
+```javascript
+setPercentileSeparation(100)
+// '100'
+setPercentileSeparation(1000)
+// '1,000'
+```
+
+### fistLetterUpper(string)
+字符串首字母大写
+- 示例
+```javascript
+fistLetterUpper('object')
+// 'Object'
+```
+
+### telFormat(string|number)
+手机号中间四位变成*
+- 示例
+```javascript
+telFormat(13836789010)
+// '138****9010'
+telFormat('13836789010')
+// '138****9010'
+```
---
+++ src/index.js
@@ -0,0 +1,76 @@
+export default class utilsVue {
+    /**
+     * 检查字符串是否为空
+     * @param {string} str - 要检查的字符串
+     * @returns {boolean} - 如果为空，返回 true；否则返回 false
+     */
+    static isEmpty(str) {
+        return !str || str.trim().length === 0;
+    }
+
+    /**
+     * 颜色格式转换：hex格式转为rgba格式
+     * @param {string} hex - hex格式颜色，例如：#C598FF
+     * @param {number} alpha - 颜色透明程度
+     * @returns {string} - 返回rgba格式的
+     */
+    static hexToRgba(hex, alpha) {
+        const hexValue = hex.replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i, (m, r, g, b) => '#' + r + r + g + g + b + b)
+        const bigint = parseInt(hexValue.substring(1), 16)
+        const r = (bigint >> 16) & 255
+        const g = (bigint >> 8) & 255
+        const b = bigint & 255
+        return `rgba(${r}, ${g}, ${b}, ${alpha})`
+    }
+
+    /**
+     * 生成指定范围随机数
+     * @param {number} min - 最小值
+     * @param {number} max - 最大值
+     * @returns {number} - 返回生产随机数
+     */
+    static randomNum (min, max){
+        return Math.floor(Math.random() * (max - min + 1 )) + min;
+    }
+
+    /**
+     * 数字转为千分位分隔
+     * @param {number} n - 传入整数
+     * @returns {string} - 返回千分位分隔的金额字符
+     */
+    static setPercentileSeparation (n){
+        if (!n) return '0'
+        let num = n.toString();
+        let len = num.length;
+        if (len <= 3) {
+            return num;
+        } else {
+            let temp = '';
+            let remainder = len % 3;
+            if (remainder > 0) { // 不是3的整数倍
+                return num.slice(0, remainder) + ',' + num.slice(remainder, len).match(/\d{3}/g).join(',') + temp;
+            } else { // 3的整数倍
+                return num.slice(0, len).match(/\d{3}/g).join(',') + temp;
+            }
+        }
+    }
+
+    /**
+     * 字符串首字母大写
+     * @param {string} str - 首字母要大写的字符串
+     * @returns {string} - 返回首字母要大写的字符串
+     */
+    static fistLetterUpper (str){
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+
+    /**
+     * 手机号中间四位变成*
+     * @param {number|string} tel - 手机号
+     * @returns {string} - 返回脱敏手机号
+     */
+    static telFormat (tel) {
+        tel = String(tel);
+        return tel.substr(0,3) + "****" + tel.substr(7);
+    }
+}
